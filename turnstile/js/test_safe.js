0b5d5877531ee4190694c7b2822b351a4a8dd5a4
Nigel Runnels-Moss <sleepyfox@gmail.com>
2020-07-02 16:52:24 +0100

    Enhancement of eSafe to allow second passcode

--- turnstile/js/test_safe.js
+++ turnstile/js/test_safe.js
@@ -1,22 +1,21 @@
 const {is, testRunner} = require('@gowerstreet/infintestimal')
 
+const LOCKED = 0
+const UNLOCKED = 5
+
 class Safe {
   constructor() {
-    this.locked = true
-    this.passcode = ['D', 'E', 'E', 'D']
-    this.state = 0
+    this.stateTable = [{A: 0, B: 0, C: 0, D: 1, E: 0, F: 0}, // 0 Starting, Locked
+                       {A: 0, B: 0, C: 0, D: 0, E: 2, F: 0}, // 1
+                       {A: 4, B: 0, C: 0, D: 0, E: 3, F: 0}, // 2
+                       {A: 0, B: 0, C: 0, D: 5, E: 0, F: 0}, // 3
+                       {A: 0, B: 0, C: 0, D: 0, E: 0, F: 5}, // 4
+                       {A: 0, B: 0, C: 0, D: 0, E: 0, F: 0}] // 5 Unlocked
+    this.state = LOCKED
   }
-  open() { return !this.locked }
-  press(key) {
-    if (key === this.passcode[this.state]) {
-      this.state++
-    } else {
-      this.state = 0
-    }
-    if (this.state === 4) { this.locked = false }
-    return null
-  }
-  shut() { return this.locked = true }
+  open() { return (this.state === UNLOCKED) }
+  press(key) { this.state = this.stateTable[this.state][key] }
+  shut() { this.state = LOCKED }
 } // end class
 
 const lockedSafe =
@@ -44,7 +43,34 @@ const lockedSafe =
           s.open()
           s.shut()
           return is(false, s.open())
-        }
-       ]]
+        }],
+       ['Entering an incorrect passcode will not open the safe',
+        () => {
+          s = new Safe()
+          s.press('B')
+          s.press('E')
+          s.press('E')
+          s.press('F')
+          return is(false, s.open())
+        }],
+       ['Entering a wrong key will abort an otherwise successful entry',
+        () => {
+          s = new Safe()
+          s.press('D')
+          s.press('E')
+          s.press('B')
+          s.press('D')
+          return is(false, s.open())
+        }],
+       ['Entering the second passcode will open the safe',
+        () => {
+          s = new Safe()
+          s.press('D')
+          s.press('E')
+          s.press('A')
+          s.press('F')
+          return is(true, s.open())
+        }]        
+       ]
 
 testRunner(lockedSafe)
